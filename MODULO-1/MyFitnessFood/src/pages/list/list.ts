17446e0f8e71337e918af4269d8beb054ad801dd
Ivan Ledinich <Ivan.Ledinich@BP4.com>
2017-11-14 10:45:33 -0300

    Proyecto ionic android

---
+++ MODULO-1/MyFitnessFood/src/pages/list/list.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedStateOfMind: any;
+  selectedTypeOfFood: any;
+  selectedDate: any;
+  selectedHour: any;
+  msgFile:string;
+  
+  constructor(private toastCtrl: ToastController) {
+   
+    var self = this;
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        self.msgFile = xhttp.responseText;
+      }
+    }
+    xhttp.open("GET", "./assets/docs/msg", true);
+    xhttp.send();
+
+
+  }
+
+  presentToast() {
+
+    let toast = this.toastCtrl.create({
+      message: this.msgFile,
+      duration: 3000,
+      position: 'bottom'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+
+}
